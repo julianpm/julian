2265fa1fb45846336bb49be34c344dbcf3778b7f
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-02-22 20:46:26 -0500

    Initial commit

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * julian Theme Customizer
+ *
+ * @package julian
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function jul_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'jul_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function jul_customize_preview_js() {
+	wp_enqueue_script( 'jul_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'jul_customize_preview_js' );
