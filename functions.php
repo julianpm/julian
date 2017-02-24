d77ad3960378b9ba01d78a6b4ba94a30f12ad584
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-02-23 21:00:05 -0500

    enqueued styles and scripts

--- functions.php
+++ functions.php
@@ -106,10 +106,12 @@ add_action( 'widgets_init', 'jul_widgets_init' );
  */
 function jul_scripts() {
 	wp_enqueue_style( 'jul-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'jul-app', get_template_directory_uri() . '/assets/css/app.css' );
 
 	wp_enqueue_script( 'jul-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'jul-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'jul-appjs', get_template_directory_uri() . '/assets/js/app.min.js', array('jquery'), true  );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
