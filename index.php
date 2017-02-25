0441ee8d81375080d1b622f6d505ae5198865ea5
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-02-24 23:12:38 -0500

    fixed markup for posts archive

--- index.php
+++ index.php
@@ -18,33 +18,23 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) :
+		if ( have_posts() ) : ?>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+			<div class="row section-padding">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post(); ?>
 
-			<?php
-			endif;
+					<div class="columns small-12 large-6">
+						<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+					</div>
+				
+				<?php
+				endwhile; ?>
+			</div>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
+			<?php 
 			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
 
@@ -52,5 +42,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,16 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		
-	</div><!-- .entry-content -->
+	<?php if ( has_post_thumbnail() ){
+		the_post_thumbnail();
+	} ?>
+	<?php
+	if ( is_single() ) :
+		the_title( '<h1 class="entry-title">', '</h1>' );
+	else :
+		the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark" class="entry-title"><h2>', '</h2></a>' );
+	endif; ?>
 </article><!-- #post-## -->
